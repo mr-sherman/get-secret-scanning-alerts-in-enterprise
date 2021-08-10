f8d934bb9ed51b92bc5810dbab5b5e43806e35d0
mr-sherman <77112096+mr-sherman@users.noreply.github.com>
2021-08-10 16:58:14 -0400

    add logic to loop through all orgs

--- get-secret-scanning-alerts.js
+++ get-secret-scanning-alerts.js
@@ -3,65 +3,69 @@
 require('dotenv').config()
 const pReduce = require('./lib/p-reduce');
 const delay = require('delay');
-const {Octokit} = require('@octokit/rest')
-
+const { Octokit } = require('@octokit/rest')
 
 var buffer = ""
 var base_url = ''
+const [, , ...args] = process.argv
 
 if (args.length > 0)
-  base_url = args[1]
+  base_url = args[0]
 
-  if (base_url.length > 0)
-  {
-    octokit = new Octokit({
-      auth: process.env.GH_AUTH_TOKEN,
-      previews: ['dorian-preview'],
-      baseUrl: base_url + '/api/v3'
-    });
-  }
-  else
-  {
-    octokit = new Octokit({
-      auth: process.env.GH_AUTH_TOKEN,
-      previews: ['dorian-preview']
-    });
-
-const [, , ...args] = process.argv
-const owner = args[0]
+if (base_url.length > 0) {
+  octokit = new Octokit({
+    auth: process.env.GH_AUTH_TOKEN,
+    previews: ['dorian-preview'],
+    baseUrl: base_url + '/api/v3'
+  });
+}
+else {
+  octokit = new Octokit({
+    auth: process.env.GH_AUTH_TOKEN,
+    previews: ['dorian-preview']
+  });
+}
 console.log("org,repo,secret_type,secret,state,resolved_at, url")
-octokit
-  .paginate(octokit.repos.listForOrg, {
-      org: owner,
-    })
-  .then(repositories =>
-    pReduce(repositories, (repository) => {
-      if (repository.archived) {
-        return Promise.resolve();
-      }
-      const repo = repository.name
+octokit.paginate("GET /organizations")
+  .then(organizations =>
+    pReduce(organizations, (organization) => {
+      owner = organization.login
 
       return octokit
-        .paginate("GET /repos/:owner/:repo/secret-scanning/alerts?per_page=100", {
-          owner: owner,
-          repo: repo
+        .paginate(octokit.repos.listForOrg, {
+          org: owner,
         })
-        .then(alerts => {
-          if (alerts.length > 0) {
+        .then(repositories =>
+          pReduce(repositories, (repository) => {
+            if (repository.archived) {
+              return Promise.resolve();
+            }
+            const repo = repository.name
 
-            pReduce(alerts, (alert) => {
-              console.log(`${owner},${repo},${alert.secret_type},${alert.secret},${alert.state},${alert.resolved_at},${alert.url}`)            }) 
-          } 
-          delay(300);
-        })
+            return octokit
+              .paginate("GET /repos/:owner/:repo/secret-scanning/alerts?per_page=100", {
+                owner: owner,
+                repo: repo
+              })
+              .then(alerts => {
+                if (alerts.length > 0) {
+
+                  pReduce(alerts, (alert) => {
+                    console.log(`${owner},${repo},${alert.secret_type},${alert.secret},${alert.state},${alert.resolved_at},${alert.url}`)
+                  })
+                }
+                delay(300);
+              })
+              .catch(error => {
+                //skip any repos that don't have any secret scanning alerts
+              })
+          })
+
+        )
         .catch(error => {
-         // console.error(`Failed for ${owner}/${repo}\n${error.message}\n${error.documentation_url}`)
-        })        
-    })
-    
-  )
-  .catch(error => {
-    console.error(`Getting repositories for organization ${owner} failed.
+          console.error(`exception 2`)
+          console.error(`Getting repositories for organization ${owner} failed.
     ${error.message} (${error.status})
     ${error.documentation_url}`)
-  })
+        })
+    }))
